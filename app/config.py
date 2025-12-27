2b03f26bf41a532af82f04b063fe0f6d90472bc9
a.b.christie <alan.christie@matildapeak.com>
2025-12-27 09:20:35 +0000

    feat: Add config (via .env)

---
+++ app/config.py
@@ -0,0 +1,6 @@
+# The application run-time configuration (using '.env').
+# Any module that relies on user-configuration parameters
+# must import this module.
+from dotenv import load_dotenv
+
+load_dotenv()
